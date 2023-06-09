3999bae6f4676cdfae187a14c392f0ceb2ba6e4d
Oliver <oliver.qin1@gmail.com>
2023-06-09 16:27:31 -0700

    add delete key capability + reorganize key data

--- components/APIKeys/index.tsx
+++ components/APIKeys/index.tsx
@@ -2,32 +2,38 @@ import Text from "@/components/Text"
 import Button from "@/components/Buttons"
 import { useState } from "react";
 import RightArrow from "../icons/RightArrow";
+import RedTrashIcon from "../icons/RedTrashIcon";
+
+interface KeyInfo {
+    key: string
+    created_date: string
+}
 
 const APIKeys = () => {
-    // TODO: Keys/created dates should likely be stored in a more secure way, but currently a placeholder
-    const [keys, setKeys] = useState<Array<string>>(["pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv"]);
-    const [keySetDates, setKeySetDates] = useState<Array<string>>(["April 30, 2023 at 5:32 PM PDT"]);
     const [isBlurred, setIsBlurred] = useState<boolean>(true);
+    const [keysInfo, setKeysInfo] = useState<KeyInfo[]>([{ key: "pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv", created_date: "April 30, 2023 at 5:32 PM PDT" }]);
 
     const createKey = () => {
-        setKeys(prevKeyList => [...prevKeyList, "pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv"]);
         let date = new Date();
         let options = { year: 'numeric' as 'numeric', month: 'long' as "long", day: 'numeric'as 'numeric', hour: 'numeric' as 'numeric', minute: 'numeric' as 'numeric', hour12: true, timeZoneName: 'short' as 'short'};
         let formattedDate = date.toLocaleString('en-US', options);
-        setKeySetDates(prevKeySetDates => [...prevKeySetDates, formattedDate.toLocaleString()]);
+        setKeysInfo(prevKeysInfo => [...prevKeysInfo, { key: "pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv", created_date: formattedDate.toLocaleString() }])
     };
+
+    const deleteKey = (indexToDelete: number) => {
+        const newKeysInfo = keysInfo.filter((item, index) => index !== indexToDelete);
+        setKeysInfo(newKeysInfo);
+    }
 
     const BlurredView = () => {
         return (
-            <>
-                <div className="relative border-2 rounded-lg border-gray-7 mb-1" onClick={() => setIsBlurred(false)}>
-                    <p className={`${isBlurred ? 'opacity-50 filter blur-md' : 'opacity-100'} font-normal text-normal py-2.5`}>pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv</p>
-                    <div className={`absolute top-3 left-20 opacity-100`} 
-                        onClick={() => setIsBlurred(false)}>
-                        CLICK TO REVEAL
-                    </div>
-                </div>      
-            </>
+            <div className="relative border-2 rounded-lg border-gray-7 mb-1 items-center" onClick={() => setIsBlurred(false)}>
+                <p className={`${isBlurred ? 'opacity-50 filter blur-md' : 'opacity-100'} font-normal text-normal py-1.5`}>pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv</p>
+                <div className={`absolute top-2 left-20 opacity-100 text-sm pl-2`} 
+                    onClick={() => setIsBlurred(false)}>
+                    CLICK TO REVEAL
+                </div>
+            </div>      
         )
     }
 
@@ -37,20 +43,25 @@ const APIKeys = () => {
                 <div className="flex flex-col p-10 border-1 bg-gray-8 rounded-lg mb-10 mr-10">
                     <Text className="font-medium text-2xl pb-5">Secret keys</Text>
                     <p className="font-small text-gray-3 text-sm pb-7">Secret keys are used for API endpoint authentication.</p>
-                    <div className="flex flex-row w-3/5 justify-between pb-5">
-                        <div className="flex flex-col pb-3">
+                    <div className="flex flex-col pb-5">
+                        <div className="flex flex-row justify-between pb-5">
                             <Text className="font-small text-gray-3 text-sm pb-3">Key</Text>
-                            {keys.map((key) => {
-                                if (isBlurred) return <BlurredView />
-                                return <p key={key} className="font-normal text-normal py-3 mb-1">{key}</p>
-                            })}
+                            <Text className="font-small text-gray-3 text-sm pb-3 pl-7">Created</Text>
+                            <Text>{" "}</Text>
                         </div>
-                        <div className="flex flex-col pb-3">
-                            <Text className="font-small justify-right text-gray-3 text-sm pb-2.5">Created</Text>
-                            {keySetDates.map((dates) => {
-                                return <p key={dates} className="font-normal text-normal py-3 mb-1">{dates}</p>
-                            })}
-                        </div>
+                        {keysInfo.length === 0 ? <p className="text-lg pb-5">No keys!</p> : ""}
+                        {keysInfo.map((keyInfo, index) => {
+                            return (
+                                <div className="flex flex-row justify-between pb-5 items-center" key={index}>
+                                    <p>{isBlurred ? <BlurredView /> : keyInfo.key}</p>
+                                    <p>{keyInfo.created_date}</p>
+                                    <div className="flex flex-row items-center hover:opacity-90" onClick={() => deleteKey(index)}>
+                                        <RedTrashIcon />
+                                        <p className="text-red pl-2">Delete</p>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                     <div className="flex flex-row text-blue-1 items-center">
                         <Button 
