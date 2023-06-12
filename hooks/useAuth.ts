7258f3a41d1c90c84b5ee095a51b54b3debc58e6
Sam Mason de Caires <sam.jbmason@gmail.com>
2023-06-12 18:45:03 +0100

    Add /login redirect when no session is found

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -10,7 +10,7 @@ export default function useAuth() {
 
   useEffect(() => {
     if (isInitialized && !session) {
-      // router.push("/login")
+      router.push("/login")
     }
   }, [isInitialized, session, router])
 
