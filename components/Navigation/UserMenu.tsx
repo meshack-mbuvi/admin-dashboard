f3dfabdb995dbdfec4421ab7c005b855ecb5d534
Sam Mason de Caires <sam.jbmason@gmail.com>
2023-06-16 18:43:06 +0100

    Stub out user menu

--- components/Navigation/UserMenu.tsx
+++ components/Navigation/UserMenu.tsx
@@ -1,18 +1,31 @@
 "use client"
 
 import { useStytchMember } from "@stytch/nextjs/b2b"
+import { useState } from "react"
 
 import User from "../icons/User"
 
 export default function UserMenu() {
   const { member } = useStytchMember()
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
 
   return (
-    <div className="flex space-x-2 rounded-full bg-gray-8 w-fit py-[10px] px-4">
-      <User className="w-3 text-gray-4" />
-      <span className="text-sm flex items-center text-gray-2">
-        {member?.name}
-      </span>
-    </div>
+    <>
+      <div
+        className="flex space-x-2 rounded-full bg-gray-8 w-fit py-[10px] px-4"
+        onClick={handleToggleMenu}
+      >
+        <User className="w-3 text-gray-4" />
+        <span className="text-sm flex items-center text-gray-2">
+          {member?.name}
+        </span>
+      </div>
+
+      <div className="rounded-2xl bg-gray-8 border-1 border-gray-7 px-4 py-4"></div>
+    </>
   )
 }
