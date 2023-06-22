9d8ae9c2b23f0752012e7e8761f7f5b6fdd79792
Sam Mason de Caires <sam.jbmason@gmail.com>
2023-06-22 19:07:10 +0100

    Fix up API keys page and components

--- components/APIKeys/index.tsx
+++ components/APIKeys/index.tsx
@@ -1,49 +1,29 @@
+"use client"
+
+import { useState } from "react"
+import { useParams } from "next/navigation"
+
 import Button from "@/components/Buttons"
 import Text from "@/components/Text"
 import Trash from "@/components/icons/Trash"
+import RightArrow from "@/components/icons/RightArrow"
+import Loading from "@/components/Loading"
+import CopyToClipboard from "@/components/CopyToClipboard"
+
 import useAuthToken from "@/hooks/useAuthToken"
 import useCreateApiKey from "@/hooks/useCreateApiKey"
 import useDeleteApiKey from "@/hooks/useDeleteApiKey"
 import useGetProjectApiKeys from "@/hooks/useGetApiKeys"
 import { formatDate } from "@/utils/formatDate"
-import clsx from "clsx"
-import { useParams } from "next/navigation"
-import { useState } from "react"
-import RightArrow from "../icons/RightArrow"
 
-const APIKeys: React.FC = () => {
-  const [isBlurred, setIsBlurred] = useState<boolean>(true)
+export default function APIKeys() {
   const { projectId } = useParams()
-  const { data } = useGetProjectApiKeys({
+  const { data, isLoading } = useGetProjectApiKeys({
     projectId,
   })
   const sessionToken = useAuthToken()
   const createMutation = useCreateApiKey(projectId)
   const deleteMutation = useDeleteApiKey(projectId)
-
-  const BlurredView = () => {
-    return (
-      <div
-        className="relative cursor-pointer border-2 rounded-lg border-gray-7 mb-1 items-center"
-        onClick={() => setIsBlurred(false)}
-      >
-        <p
-          className={clsx(
-            "font-normal text-normal py-1.5",
-            isBlurred ? "opacity-50 filter blur-md" : "opacity-100"
-          )}
-        >
-          pk_test_QmMWqQ6TJ0t6qTkAtIJP1fhv
-        </p>
-        <div
-          className={`absolute top-2 left-20 opacity-100 text-sm pl-2`}
-          onClick={() => setIsBlurred(false)}
-        >
-          CLICK TO REVEAL
-        </div>
-      </div>
-    )
-  }
 
   const handleCreateAccessKey = () => {
     if (sessionToken) {
@@ -72,68 +52,107 @@ const APIKeys: React.FC = () => {
 
   return (
     <div>
-      <div className="flex font-sans flex-col h-full w-full">
-        <div className="flex flex-col p-10 border-1 bg-gray-8 rounded-lg mb-10 mr-10">
-          <Text className="font-medium text-2xl pb-5">Secret keys</Text>
-          <p className="font-small text-gray-3 text-sm pb-7">
-            Secret keys are used for API endpoint authentication.
-          </p>
-          <div className="flex flex-col pb-5">
-            {data && data.length ? (
-              <div className="grid grid-cols-2 pb-5">
-                <div className="grid grid-cols-2 gap-x-8">
-                  <Text className="font-small text-gray-3 text-sm pb-3">
-                    Key
-                  </Text>
-                  <Text className="font-small text-gray-3 text-sm pb-3">
-                    Created
-                  </Text>
-                </div>
-                <div /> {/* Empty div for spacing */}
+      <div className="flex flex-col p-10 border-1 bg-gray-8 rounded-lg mb-10 mr-10">
+        <Text className="font-medium text-2xl pb-5">Secret keys</Text>
+        <p className="font-small text-gray-3 text-sm pb-7">
+          Secret keys are used for API endpoint authentication.
+        </p>
+        <div className="flex flex-col pb-5">
+          {isLoading ? (
+            [...Array(6)].map((_, i) => (
+              <div className="flex gap-x-8 py-4" key={i}>
+                <Loading className="w-1/4" />
+                <Loading className=" w-1/4" />
               </div>
-            ) : (
-              <p className="text-lg pb-5">
-                There are currently no API keys for this project
-              </p>
-            )}
-            {data &&
-              data.map(({ AccessKey }, index) => {
-                return (
-                  <div className="grid grid-cols-2 pb-5" key={index}>
-                    <div className="grid grid-cols-2 gap-x-8">
-                      {isBlurred ? (
-                        <BlurredView />
-                      ) : (
-                        <Text className="items-center">{AccessKey?.key}</Text>
-                      )}
-                      <Text className="flex items-center">
-                        {formatDate(AccessKey?.createdAt)}
-                      </Text>
-                    </div>
-                    <div
-                      className="flex justify-end mr-16 flex-row cursor-pointer items-center hover:opacity-90"
-                      onClick={() => handleDeleteAccessKey(AccessKey?.id)}
-                    >
-                      <Trash className="w-3.5 h-4 text-red" />
-                      <Text className="text-red pl-2">Delete</Text>
-                    </div>
+            ))
+          ) : data && data.length ? (
+            <div className="grid grid-cols-3 pb-5">
+              <div className="col-span-2 grid grid-cols-2 gap-x-8">
+                <Text className="font-small text-gray-3 text-sm pb-3">Key</Text>
+                <Text className="font-small text-gray-3 text-sm pb-3">
+                  Created
+                </Text>
+              </div>
+              <div /> {/* Empty div for spacing */}
+            </div>
+          ) : (
+            <p className="text-lg pb-5">
+              There are currently no API keys for this project
+            </p>
+          )}
+          {data &&
+            data.map(({ AccessKey }, index) => {
+              return (
+                <div className="grid grid-cols-3 pb-5" key={index}>
+                  <div className="col-span-2 grid grid-cols-2 gap-x-8 items-center">
+                    <BlurredView>
+                      <div className="flex">
+                        <Text className="font-mono">{AccessKey?.key}</Text>
+                        <CopyToClipboard
+                          text={AccessKey?.key}
+                          className="ml-auto"
+                        />
+                      </div>
+                    </BlurredView>
+
+                    <Text className="flex font-mono flex-shrink-0">
+                      {formatDate(AccessKey?.createdAt)}
+                    </Text>
                   </div>
-                )
-              })}
-          </div>
-          <div className="flex flex-row text-blue-1 items-center">
-            <Button
-              className="font-normal text-normal pr-1"
-              onClick={handleCreateAccessKey}
-            >
-              Create a new key
-            </Button>
-            <RightArrow className="w-3 h-3" />
-          </div>
+                  <div
+                    className="flex justify-end mr-16 flex-row cursor-pointer items-center hover:opacity-90"
+                    onClick={() => handleDeleteAccessKey(AccessKey?.id)}
+                  >
+                    <Trash className="w-3.5 h-4 text-red" />
+                    <Text className="text-red pl-2">Delete</Text>
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+        <div className="flex flex-row text-blue-1 items-center">
+          <Button
+            className="font-normal text-normal pr-1"
+            onClick={handleCreateAccessKey}
+          >
+            Create a new key
+          </Button>
+          <RightArrow className="w-3 h-3" />
         </div>
       </div>
     </div>
   )
 }
 
-export default APIKeys
+interface BlurredViewProps {
+  children: React.ReactNode
+}
+
+const BlurredView = (props: BlurredViewProps) => {
+  const { children } = props
+
+  const [isBlurred, setIsBlurred] = useState<boolean>(true)
+
+  return (
+    <div
+      className="relative cursor-pointer border-2 rounded-lg border-gray-7 mb-1 text-center py-1.5 px-2"
+      onClick={() => setIsBlurred(false)}
+    >
+      {isBlurred ? (
+        <>
+          <p className="opacity-50 filter blur-sm overflow-hidden text-ellipsis whitespace-nowrap">
+            Im a fake test key gkdkdfdsfsfsd
+          </p>
+          <div
+            className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center opacity-100 text-sm font-mono"
+            onClick={() => setIsBlurred(false)}
+          >
+            CLICK TO REVEAL
+          </div>
+        </>
+      ) : (
+        children
+      )}
+    </div>
+  )
+}
