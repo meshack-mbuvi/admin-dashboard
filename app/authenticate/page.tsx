2179d741b9eace023cd7ad38b2d391d2fa4f76e7
Sam Mason de Caires <sam.jbmason@gmail.com>
2023-06-23 17:42:45 +0100

    Handle auth errors and faster redirect on login page

--- app/authenticate/page.tsx
+++ app/authenticate/page.tsx
@@ -2,13 +2,23 @@
 
 import { useStytchB2BClient, useStytchMemberSession } from "@stytch/nextjs/b2b"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
+import Button from "@/components/Buttons"
+import Logo from "@/components/icons/Logo"
+
+type ErrorType = "expiredLink" | "generic"
 
 // DEV: This page is configured as the login callback url from Stytch
 export default function Authenticate() {
   const stytch = useStytchB2BClient()
   const { session } = useStytchMemberSession()
   const router = useRouter()
+  const [error, setError] = useState<ErrorType | null>("expiredLink")
+
+  const handleBackToLogin = () => {
+    router.push("/")
+  }
 
   useEffect(() => {
     const authenticate = async () => {
@@ -25,21 +35,59 @@ export default function Authenticate() {
         router.push("/")
         return
       }
-      // Authenticate the user with the token
-      await stytch.magicLinks.authenticate({
-        magic_links_token: token,
-        // DEV: 7 days in minutes
-        session_duration_minutes: 10080,
-      })
 
-      router.push("/projects")
+      try {
+        // Authenticate the user with the token
+        await stytch.magicLinks.authenticate({
+          magic_links_token: token,
+          // DEV: 7 days in minutes
+          session_duration_minutes: 10080,
+        })
+
+        router.push("/projects")
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          error.message.includes("unable_to_auth_magic_link")
+        ) {
+          return setError("expiredLink")
+        }
+
+        return setError("generic")
+      }
     }
 
     authenticate()
   }, [stytch, session, router])
 
+  if (error === "expiredLink") {
+    return (
+      <div className="text-center w-full mt-24">
+        <Logo className="w-16 mx-auto mb-8" />
+        <p className="text-gray-2 text-2xl mb-6">
+          This link is expired or has already been used, try logging in again
+        </p>
+
+        <Button onClick={handleBackToLogin}>Back to Login</Button>
+      </div>
+    )
+  }
+
+  if (error === "generic") {
+    return (
+      <div className="text-center w-full mt-24">
+        <Logo className="w-16 mx-auto mb-8" />
+        <p className="text-gray-2 text-2xl mb-6">
+          Something went wrong, try logging in again
+        </p>
+
+        <Button onClick={handleBackToLogin}>Back to Login</Button>
+      </div>
+    )
+  }
+
   return (
-    <div className="  font-medium text-2xl text-center w-full mt-24">
+    <div className="font-medium text-2xl text-center w-full mt-24">
       Loading...
     </div>
   )
---
+++ components/LoginForm.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { FormEvent, useState } from "react"
+import { useStytchB2BClient } from "@stytch/nextjs/b2b"
+
+import { getAuthRedirectURL } from "@/utils/environment"
+import Section from "./Section"
+import Label from "./Label"
+import Input from "./Input"
+
+const VALID_DOMAINS = ["syndicate.io", "nike.com"]
+
+const DOMAIN_CONNECTIONS = {
+  "syndicate.io": "organization-test-2f932663-014a-438c-8b2f-11722cdaae3a",
+  "nike.com": "",
+}
+
+export default function LoginForm() {
+  const [emailAddress, setEmailAddress] = useState<string>("")
+  const [loginContinued, setLoginContinued] = useState<boolean>(false)
+  const stytch = useStytchB2BClient()
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const [_, domain] = emailAddress.split("@")
+    if (!VALID_DOMAINS.includes(domain)) {
+      return alert("Please use a valid email address")
+    }
+
+    stytch.magicLinks.email.loginOrSignup({
+      email_address: emailAddress,
+      organization_id:
+        DOMAIN_CONNECTIONS[domain as keyof typeof DOMAIN_CONNECTIONS],
+      // Upon successful login and receiving email invite, redirect the user to the specified URL
+      login_redirect_url: getAuthRedirectURL(),
+    })
+    setLoginContinued(true)
+  }
+
+  return loginContinued ? (
+    <h2 className="font-medium text-2xl mb-14">
+      Check your email for Dashboard Access
+    </h2>
+  ) : (
+    <>
+      <h2 className="font-medium text-2xl mb-14">Welcome to Syndicate</h2>
+      <Section className="p-6 w-full">
+        <form onSubmit={handleLogin}>
+          <Label className="block mb-4" htmlFor="email-address">
+            Enter your email address
+          </Label>
+
+          <Input
+            id="email-address"
+            className="block w-full mb-6"
+            placeholder="Email address"
+            onChange={(e) => setEmailAddress(e.target.value)}
+            value={emailAddress}
+          />
+
+          <button
+            type="submit"
+            className="bg-white rounded-lg w-full py-4 text-black text-base font-semibold hover:opacity-90"
+          >
+            Continue
+          </button>
+        </form>
+      </Section>
+    </>
+  )
+}
