8f043264cdfd815fc9b2bc93f5e9cf6af2af27af
Oliver <oliver.qin1@gmail.com>
2023-06-27 08:38:44 -0700

    remove Nike Logo, replace with dynamic orgnaization name

--- components/Navigation/Breadcrumbs.tsx
+++ components/Navigation/Breadcrumbs.tsx
@@ -1,21 +1,27 @@
 "use client"
 
 import Link from "next/link"
-import NikeBrand from "@/components/icons/NikeBrand"
 import useGetOrganization from "@/hooks/useGetOrganization"
+import useGetProjects from "@/hooks/useGetProjects"
+import Loading from "../Loading"
 
-export default /* async */ function Breadcrumbs() {
-  const { data, isLoading } = useGetOrganization()
+export default function Breadcrumbs() {
+  const { data: organizationData, isLoading: isOrganizationDataLoading } = useGetOrganization()
+  const { data: projectsData, isLoading: isProjectsDataLoading } = useGetProjects()
 
-  console.log("useGetOrganization data", data)
+  console.log("useGetOrganization data", organizationData)
+  console.log("useGetProjects data", projectsData)
 
   return (
     <ul className="breadcrumb flex">
       <li className="cursor-pointer flex space-x-3 py-2 text-sm active pl-3 bg-gray-8 rounded-l-full">
         <Link href="/projects" className="flex">
-          <NikeBrand className="w-7" />
           <span className="flex text-center items-center text-gray-2">
-            Nike
+            {isOrganizationDataLoading ? (
+              <Loading className="h-6 w-24" />
+            ) : (
+              organizationData?.name
+            )}
           </span>
         </Link>
       </li>
