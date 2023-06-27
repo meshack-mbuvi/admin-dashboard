266ec692eade8f98038c17b780b2eaad2533e9f4
Oliver <oliver.qin1@gmail.com>
2023-06-27 08:51:10 -0700

    add dynamic project name

--- components/Navigation/Breadcrumbs.tsx
+++ components/Navigation/Breadcrumbs.tsx
@@ -2,15 +2,16 @@
 
 import Link from "next/link"
 import useGetOrganization from "@/hooks/useGetOrganization"
-import useGetProjects from "@/hooks/useGetProjects"
 import Loading from "../Loading"
+import { useParams } from "next/navigation"
+import useGetProjectById from "@/hooks/useGetProjectById"
 
 export default function Breadcrumbs() {
+  const { projectId } = useParams();
   const { data: organizationData, isLoading: isOrganizationDataLoading } = useGetOrganization()
-  const { data: projectsData, isLoading: isProjectsDataLoading } = useGetProjects()
-
-  console.log("useGetOrganization data", organizationData)
-  console.log("useGetProjects data", projectsData)
+  const { data: projectData, isLoading: isProjectDataLoading } = useGetProjectById({
+    projectId
+  })
 
   return (
     <ul className="breadcrumb flex">
@@ -26,11 +27,15 @@ export default function Breadcrumbs() {
         </Link>
       </li>
 
-      {/* {projectId && (
+      {projectId && (
         <li className="flex items-center space-x-3 pl-10 py-2.5 active text-sm bg-gray-8 text-gray-2">
-          .Swoosh
+          {isProjectDataLoading ? (
+              <Loading className="h-6 w-30" />
+            ) : (
+              projectData?.name
+            )}
         </li>
-      )} */}
+      )}
     </ul>
   )
 }
