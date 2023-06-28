6b934c697b10bebd29261abea7b0890592df4ad1
Meshack Mbuvi <meshmbuvi@gmail.com>
2023-06-28 12:54:17 +0300

    Move projects to its own folder (#59)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,51 +1,11 @@
 "use client"
 
 import Button, { DarkButtonStyles } from "@/components/Buttons"
-import Loading from "@/components/Loading"
+import Projects from "@/components/Projects"
 import { Tab } from "@/components/Tab"
 import Add from "@/components/icons/Add"
-import useGetProjects from "@/hooks/useGetProjects"
 import clsx from "clsx"
 import { useState } from "react"
-import ProjectRow from "./components/ProjectRow"
-
-const Projects = () => {
-  const { data, isLoading } = useGetProjects()
-
-  return (
-    <>
-      <div className="text-2xl text-gray-1 pl-1 mb-7">Projects</div>
-      <div className="flex flex-col space-y-7">
-        {/* Header */}
-        <div className="flex">
-          <div className="w-1/4 text-left italic text-sm text-gray-4">Name</div>
-        </div>
-
-        {/* Project list */}
-        {isLoading && (
-          <div>
-            <Loading className="h-6 w-48" />
-            <div className="border-b border-gray-7 my-6" />
-            <Loading className="h-6 w-52" />
-            <div className="border-b border-gray-7 my-6" />
-            <Loading className="h-6 w-60" />
-            <div className="border-b border-gray-7 my-6" />
-            <Loading className="h-6 w-48" />
-            <div className="border-b border-gray-7 my-6" />
-          </div>
-        )}
-
-        {data && (
-          <div className="-ml-7 -mr-7 flex flex-col">
-            {data.map(({ name, id }) => (
-              <ProjectRow key={id} name={name} projectId={id} />
-            ))}
-          </div>
-        )}
-      </div>
-    </>
-  )
-}
 
 export default function Dashboard() {
   const tabHeaders = ["Projects", "People"]
---
+++ components/Projects/index.tsx
@@ -0,0 +1,41 @@
+import ProjectRow from "@/app/dashboard/components/ProjectRow"
+import useGetProjects from "@/hooks/useGetProjects"
+import Loading from "../Loading"
+
+export default function Projects() {
+  const { data, isLoading } = useGetProjects()
+
+  return (
+    <>
+      <div className="text-2xl text-gray-1 pl-1 mb-7">Projects</div>
+      <div className="flex flex-col space-y-7">
+        {/* Header */}
+        <div className="flex">
+          <div className="w-1/4 text-left italic text-sm text-gray-4">Name</div>
+        </div>
+
+        {/* Project list */}
+        {isLoading && (
+          <div>
+            <Loading className="h-6 w-48" />
+            <div className="border-b border-gray-7 my-6" />
+            <Loading className="h-6 w-52" />
+            <div className="border-b border-gray-7 my-6" />
+            <Loading className="h-6 w-60" />
+            <div className="border-b border-gray-7 my-6" />
+            <Loading className="h-6 w-48" />
+            <div className="border-b border-gray-7 my-6" />
+          </div>
+        )}
+
+        {data && (
+          <div className="-ml-7 -mr-7 flex flex-col">
+            {data.map(({ name, id }) => (
+              <ProjectRow key={id} name={name} projectId={id} />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
