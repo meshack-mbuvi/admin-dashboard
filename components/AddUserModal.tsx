09084bfd52c98bdfaf36ee4a4335c66a4295c648
Oliver <oliver.qin1@gmail.com>
2023-07-10 08:30:13 -0700

    initial changes of design + backend call

--- components/AddUserModal.tsx
+++ components/AddUserModal.tsx
@@ -1,30 +1,51 @@
 import React, { useState } from 'react';
+import useAuthToken from '@/hooks/useAuthToken';
+import { useParams } from 'next/navigation';
+import useCreateApiKey from '@/hooks/useCreateApiKey';
 
 type AddUserModalProps = {
   show: boolean,
   onClose: () => void,
 };
 
-
 const AddUserModal : React.FC<AddUserModalProps> = ({ show, onClose }) => {
+    const sessionToken = useAuthToken()
+    const { projectId } = useParams()
+    const createMutation = useCreateApiKey(projectId)
+
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    
     if (!show) return null;
+
+    const handleRequest = () => {
+        if (sessionToken) {
+            createMutation.mutate({
+                method: "POST",
+                sessionToken,
+                endpointPath: `/admin/user`,
+                // Delete Role and set it to admin for now
+                body: JSON.stringify({ name, email, role: "admin" })
+            })
+        }
+    }
+
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="flex flex-col justify-center items-left w-2/5 mx-auto bg-gray-8 p-8">
-                <p className="font-sans font-medium text-2xl text-gray-1 mb-7">Invite to project</p>
+                <p className="font-sans font-medium text-2xl text-gray-1 mb-7">Invite user</p>
                 <div className="flex flex-col justify-center items-left">
                     <p className="font-sans font-medium text-white text-sm mb-2 bg-dark">Name</p>
-                    <input className="border border-gray-7 text-gray-4 bg-inherit rounded-lg px-4 py-3.5 mb-7" placeholder="Example Person" />
+                    <input value={name} onChange={(e => setName(e.target.value))} className="border border-gray-7 text-gray-4 bg-inherit rounded-lg px-4 py-3.5 mb-7" placeholder="Example Person" />
                 </div>
                 <div className="flex flex-col justify-center items-left">
                     <p className="font-sans font-medium  text-white text-sm mb-2">Email Address</p>
-                    <input className="text-base border border-gray-7 text-gray-4 bg-inherit rounded-lg px-4 py-3.5 mb-7" placeholder="example@example.com" />
+                    <input value={email} onChange={(e => setEmail(e.target.value))} className="text-base border border-gray-7 text-gray-4 bg-inherit rounded-lg px-4 py-3.5 mb-7" placeholder="example@example.com" />
                 </div>
-                <div className="flex flex-col justify-center items-left">
-                    <p className="font-sans font-medium text-white text-sm mb-2">Role</p>
-                    <input className="border border-gray-7 text-gray-4 bg-inherit rounded-lg px-4 py-3.5 mb-7" placeholder="Select a role" />
-                </div>
-                <input type="button" onClick={() => onClose()} className="text-black font-sans font-medium bg-white rounded-lg px-8 py-3.5 mb-7" value="Invite to project" />
+                <input type="button" onClick={() => {
+                    handleRequest();
+                    onClose();
+                }} className="text-black font-sans font-medium bg-white rounded-lg px-8 py-3.5 mb-7 hover:opacity-90" value="Invite to project" />
             </div>
         </div>
     )
