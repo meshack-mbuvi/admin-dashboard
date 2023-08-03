c4b2ad40fbd5798a0c61897773a71f3618bbc28e
Isaac <isaac@syndicate.io>
2023-08-03 11:01:00 -0400

    Hook up API call for creating project

--- components/CreateProjectModal.tsx
+++ components/CreateProjectModal.tsx
@@ -1,6 +1,5 @@
 import useAuthToken from "@/hooks/useAuthToken"
 import useCreateUser from "@/hooks/useCreateUser"
-import { ResponseError } from "@/utils/gatewayFetch"
 import React, { useEffect, useMemo, useState } from "react"
 import Input from "./Input"
 import Select from "./Select"
@@ -8,6 +7,8 @@ import Modal from "./Modal"
 import { Spinner } from "./Spinner"
 import SuccessCheckMark from "./icons/successCheckMark"
 import NetworkDropdown from "./NetworkDropdown"
+import useGetOrganization from "@/hooks/useGetOrganization"
+import { useRouter } from "next/navigation"
 
 type CreateProjectModalProps = {
   show: boolean
@@ -16,6 +17,7 @@ type CreateProjectModalProps = {
 
 const PendingStatusText = "Creating project"
 const SuccessStatusText = "Project created"
+const ErrorStatusText = "Error creating project"
 
 const enviromentOptions = [
   { id: 0, label: "Staging" },
@@ -26,9 +28,13 @@ const CreateProjectModal: React.FC<CreateProjectModalProps> = ({
   show,
   onClose,
 }) => {
+  const router = useRouter()
   const sessionToken = useAuthToken()
-  const { isError, isSuccess, mutate, error, reset } = useCreateUser()
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const { data: organizationData, isLoading: isOrganizationDataLoading } =
+    useGetOrganization()
+
+  const { data, isError, isSuccess, mutate, isLoading, reset } = useCreateUser()
+  const [newProjectId, setNewProjectId] = useState<string>("")
 
   const [name, setName] = useState<string>("")
   const [enviroment, setEnviroment] = useState<{
@@ -37,39 +43,32 @@ const CreateProjectModal: React.FC<CreateProjectModalProps> = ({
   }>()
   const [network, setNetwork] = useState<number>(0)
 
-  const [errorMessage, setErrorMessage] = useState<string>("")
-
   useEffect(() => {
-    let _statusText = ""
-    if (isError && error.status === 409) {
-      _statusText = "Project already exists"
-    }
-    setErrorMessage(_statusText)
-    setIsSubmitting(false)
-  }, [isError, error, isSuccess, name])
+    if (!data) return
+    data?.json().then((data) => {
+      setNewProjectId(data.id)
+    })
+  }, [data])
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    reset()
     const { value } = e.target
     setName(value)
   }
 
   const handleRequest = () => {
     if (sessionToken) {
-      setIsSubmitting(true)
-      //   mutate({
-      //     method: "POST",
-      //     sessionToken,
-      //     endpointPath: "/admin/project",
-      //     body: JSON.stringify({
-      //       name: name,
-      //       enviroment: name,
-      //       chainId: "admin",
-      //       numWallets: 3,
-      //     }),
-      //   })
-      console.log("TODO: Create project API call")
-      console.log({ name, enviroment, network })
+      mutate({
+        method: "POST",
+        sessionToken,
+        endpointPath: "/admin/project",
+        body: JSON.stringify({
+          organizationId: organizationData?.organization.id,
+          name: name,
+          enviroment: enviroment?.label,
+          chainId: network,
+          numWallets: 1,
+        }),
+      })
     }
   }
 
@@ -82,69 +81,84 @@ const CreateProjectModal: React.FC<CreateProjectModalProps> = ({
           setName("")
           setEnviroment(undefined)
           setNetwork(0)
+          setNewProjectId("")
+          reset()
           onClose()
         }}
       >
         <div className="flex flex-col justify-center items-left bg-gray-8 my-4">
-          <p className="font-sans font-medium text-2xl text-gray-1 mb-7">
-            New Project
-          </p>
-          <div className="flex flex-col justify-center items-left mb-7">
-            <p className="font-sans font-medium text-white text-sm mb-2 bg-dark">
-              Name
-            </p>
-            <Input
-              placeholder="My Project XYZ"
-              value={name}
-              onChange={(e) => handleNameChange(e)}
-            />
-            {errorMessage && (
-              <p className="font-sans font-medium text-red text-sm mt-2 mb-7">
-                {errorMessage}
-              </p>
-            )}
-          </div>
-          <div className="flex flex-col justify-center items-left">
-            <p className="font-sans font-medium text-white text-sm mb-2">
-              Enviroment
-            </p>
-            <Select
-              options={enviromentOptions}
-              placeholder="Select enviroment type"
-              selected={enviroment}
-              setSelected={setEnviroment}
-            />
-            <br></br>
-          </div>
-          <div className="flex flex-col justify-center items-left">
-            <NetworkDropdown
-              currentNetwork={network}
-              setCurrentNetwork={setNetwork}
-              placeholder="Select preliminary network"
-              above={true}
-            />
-            <br></br>
-          </div>
-          {isSubmitting ? (
+          {isLoading ? (
             <div className="flex w-full align-middle justify-center">
               <span className="mr-4">{PendingStatusText}</span>
               <Spinner className="h-6 w-6 text-blue-neptune" />
             </div>
-          ) : isSuccess ? (
-            <div className="flex align-middle justify-center">
-              <span className="mr-4 text-green">{SuccessStatusText}</span>
-              <SuccessCheckMark className="h-6 w-6 text-green" />
+          ) : isError ? (
+            <div className="flex w-full align-middle justify-center">
+              <span className="mr-4 text-red">{ErrorStatusText}</span>
             </div>
+          ) : isSuccess ? (
+            <>
+              <div className="flex align-middle justify-center mb-7">
+                <span className="mr-4 text-green">{SuccessStatusText}</span>
+                <SuccessCheckMark className="h-6 w-6 text-green" />
+              </div>
+              {newProjectId && (
+                <input
+                  type="button"
+                  onClick={() => {
+                    router.push(`/dashboard/${newProjectId}/transactions`)
+                  }}
+                  className="text-black font-sans disabled:bg-opacity-60 cursor-pointer disabled:cursor-not-allowed font-medium bg-white rounded-lg px-8 py-3.5"
+                  value="Go to project"
+                />
+              )}
+            </>
           ) : (
-            <input
-              type="button"
-              disabled={!!errorMessage || !name || !enviroment || !network}
-              onClick={() => {
-                handleRequest()
-              }}
-              className="text-black font-sans disabled:bg-opacity-60 disabled:cursor-not-allowed font-medium bg-white rounded-lg px-8 py-3.5"
-              value="Create project"
-            />
+            <>
+              <p className="font-sans font-medium text-2xl text-gray-1 mb-7">
+                New Project
+              </p>
+              <div className="flex flex-col justify-center items-left mb-7">
+                <p className="font-sans font-medium text-white text-sm mb-2 bg-dark">
+                  Name
+                </p>
+                <Input
+                  placeholder="My Project XYZ"
+                  value={name}
+                  onChange={(e) => handleNameChange(e)}
+                />
+              </div>
+              <div className="flex flex-col justify-center items-left mb-7">
+                <p className="font-sans font-medium text-white text-sm mb-2">
+                  Enviroment
+                </p>
+                <Select
+                  options={enviromentOptions}
+                  placeholder="Select enviroment type"
+                  selected={enviroment}
+                  setSelected={setEnviroment}
+                />
+              </div>
+              <div className="flex flex-col justify-center items-left mb-7">
+                <NetworkDropdown
+                  currentNetwork={network}
+                  setCurrentNetwork={setNetwork}
+                  placeholder="Select preliminary network"
+                  above={true}
+                />
+              </div>
+              <input
+                type="button"
+                disabled={
+                  !name || !enviroment || !network || isOrganizationDataLoading
+                }
+                onClick={() => {
+                  handleRequest()
+                }}
+                className="text-black font-sans disabled:bg-opacity-60 cursor-pointer disabled:cursor-not-allowed font-medium bg-white rounded-lg px-8 py-3.5"
+                value="Create project"
+              />
+            </>
           )}
         </div>
       </Modal>
