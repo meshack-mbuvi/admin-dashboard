66b889ce31e6d10587152b7461c490a5ce0ad681
Isaac <isaac@syndicate.io>
2023-09-07 13:04:51 -0400

    Adding 1442 chain

--- utils/getNetwork.ts
+++ utils/getNetwork.ts
@@ -1,9 +1,17 @@
-import { goerli, mainnet, polygon, Chain, polygonMumbai } from "viem/chains"
+import {
+  goerli,
+  mainnet,
+  polygon,
+  Chain,
+  polygonMumbai,
+  polygonZkEvmTestnet,
+} from "viem/chains"
 
 export const networks = {
   1: mainnet,
   5: goerli,
   137: polygon,
+  1442: polygonZkEvmTestnet,
   80001: polygonMumbai,
 } as const
 
