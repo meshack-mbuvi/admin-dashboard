143ebcf7ccf23b63654ae99f98b08f266a829ff6
daniel kipkemei <dann.professional@gmail.com>
2023-09-08 16:13:44 +0300

    copy update (#160)

--- components/Shared/ResourceID.tsx
+++ components/Shared/ResourceID.tsx
@@ -1,9 +1,8 @@
 import clsx from "clsx"
-import { MouseEvent, useEffect, useState } from "react"
-import { Tooltip } from "react-tooltip"
 
 import IdIcon from "@/components/icons/ID"
 import Clipboard from "@/components/icons/Clipboard"
+import CopyToClipboard from "@/components/CopyToClipboard"
 
 export default function ResourceID(props: {
   ID: string
@@ -13,49 +12,20 @@ export default function ResourceID(props: {
 }) {
   const { ID, className, fullView, copyIcon = true } = props
 
-  const [copied, setCopied] = useState<boolean>(false)
-
-  const copyContent = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    e.stopPropagation()
-    try {
-      await navigator.clipboard.writeText(ID)
-      setCopied(true)
-    } catch (err) {
-      setCopied(false)
-    }
-  }
-
-  useEffect(() => {
-    if (copied) {
-      setTimeout(() => {
-        setCopied(false)
-      }, 2000)
-    }
-  }, [copied])
-
   return (
     <span className={clsx(className, copyIcon && "flex space-x-4 group")}>
       {copyIcon && fullView && (
         <span className="overflow-x-hidden text-ellipsis">{ID}</span>
       )}
-      <button className="relative flex align-center py-1" onClick={copyContent}>
-        <span
-          data-tooltip-id={"resource-" + ID}
-          data-tooltip-content={copied ? "Copied" : ""}
-          data-tooltip-place="top"
-          className="h-5"
-        >
-          {copyIcon && fullView ? (
-            <Clipboard className="cursor-pointer w-4 invisible group-hover:visible" />
-          ) : fullView ? (
-            <span className="overflow-x-hidden text-ellipsis">{ID}</span>
-          ) : (
-            <IdIcon className="cursor-pointer w-5" />
-          )}
-        </span>
-      </button>
-      <Tooltip id={"resource-" + ID} />
+      <CopyToClipboard text={ID} copyId={ID} tooltipCopiedText="ID Copied">
+        {copyIcon && fullView ? (
+          <Clipboard className="cursor-pointer w-4 invisible group-hover:visible" />
+        ) : fullView ? (
+          <span className="overflow-x-hidden text-ellipsis">{ID}</span>
+        ) : (
+          <IdIcon className="cursor-pointer w-5" />
+        )}
+      </CopyToClipboard>
     </span>
   )
 }
