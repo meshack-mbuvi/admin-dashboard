173c3d279cf0085819309e488093dbc21f51751f
Meshack Mbuvi <meshmbuvi@gmail.com>
2023-10-17 16:12:48 +0300

    Gray-out form fields when unable to take action. (#204)
    * Gray out form fields when unable to take action.
    
    * Make text on submit button light gray when button is disabled.

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -7,7 +7,7 @@ export const DarkButtonStyles =
   "py-3 w-fit rounded-full h-fit px-8 bg-gray-8 border-gray-7 border disabled:bg-gray-7 disabled:text-gray-24 text-center hover:opacity-80 disabled:cursor-not-allowed"
 
 export const SubmitButtonStyles =
-  "bg-white rounded-lg w-full py-4 text-black text-base font-semibold hover:opacity-90 disabled:bg-gray-7 hover:disabled:opacity-100 disabled:cursor-not-allowed"
+  "bg-white rounded-lg w-full py-4 text-black disabled:text-gray-5 text-base font-semibold hover:opacity-90 disabled:bg-gray-7 hover:disabled:opacity-100 disabled:cursor-not-allowed"
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
